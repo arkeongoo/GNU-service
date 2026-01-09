b142ab211ef3ba4b0315d8be54fa8f11b1e540d7
arkeongoo <arkeongoo@naver.com>
2026-01-09 14:54:09 +0900

    Delete GNU service directory

--- GNU service/src/shared/lib/geo.js
+++
@@ -1,7 +0,0 @@
-// src/shared/lib/geo.js
-export function inBounds(lat, lon, bounds) {
-  // bounds: Leaflet LatLngBounds
-  const sw = bounds.getSouthWest();
-  const ne = bounds.getNorthEast();
-  return lat >= sw.lat && lat <= ne.lat && lon >= sw.lng && lon <= ne.lng;
-}
--- GNU service/src/store/mapUiStore.js
+++
@@ -1,8 +0,0 @@
-// src/store/mapUiStore.js
-import { create } from "zustand";
-
-export const useMapUiStore = create((set) => ({
-  showCctv: true,
-  setShowCctv: (v) => set({ showCctv: v }),
-  toggleCctv: () => set((s) => ({ showCctv: !s.showCctv })),
-}));
